724545af5a3cfa369b3540365055a35046328060
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-24 14:50:46 -0400

    added image, js updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../logo.svg';
 import weedJarPhoto from '../images/weed-jar.jpg';
+import givingHandPhoto from '../images/giving-cannabis.png';
 
 const HomePage = () => {
   return (
@@ -16,16 +17,17 @@ const HomePage = () => {
         </nav>
       </header>
       <main className="bg-white rounded-lg shadow">
-        <section className="flex justify-between align-center">
+        <section className="flex justify-between items-center">
           <div className="w-1/2 border-white border-2 rounded-lg">
             <img src={weedJarPhoto} alt="Cannabis jar on kitchen counter" className="rounded-tl-lg rounded-bl-lg flex-grow object-cover h-full" />
           </div>
-          <div className="p-10 w-1/2 flex flex-col gap-4 justify-center">
+          <div className="p-20 w-1/2 flex flex-col gap-4 justify-center items-center relative">
+          <img src={givingHandPhoto} alt="Giving hands with cannabis leaf in palm" className="absolute w-1/5 -top-2 right-24" />
             <h1 className="custom-text-green text-7xl font-bold">Affordable cannabis for all</h1>
             <p className="font-semibold text-lg">The only place here to help you get all your cannabis products and prescriptions at prices you can afford and sustain.</p>
-            <div className="flex gap-2 mt-4">
-              <a href="#/" className="py-4 px-24 bg-blue-300 text-center rounded-md custom-bg-green text-white font-medium">Button</a>
-              <a href="#/" className="py-4 px-24 bg-blue-300 text-center rounded-md custom-bg-darkblue text-white font-medium">Button</a>
+            <div className="flex gap-2 mt-4 w-full">
+              <a href="#/" className="py-4 bg-blue-300 text-center rounded-md custom-bg-green text-white font-medium w-full">Button</a>
+              <a href="#/" className="py-4 bg-blue-300 text-center rounded-md custom-bg-darkblue text-white font-medium w-full">Button</a>
             </div>
           </div>
         </section>
