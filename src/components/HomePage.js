a9d5944cba0d6f8a6a3e923127b2eb4c1eb9cbb2
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-24 15:02:58 -0400

    svg add, js update

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../logo.svg';
 import weedJarPhoto from '../images/weed-jar.jpg';
-import givingHandPhoto from '../images/giving-cannabis.png';
+import givingHandPhoto from '../images/giving-cannabis.svg';
 
 const HomePage = () => {
   return (
