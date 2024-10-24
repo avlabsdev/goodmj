e051f73147af2b455d7efa12d0cbf57c0af4b51f
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-24 13:52:11 -0400

    js updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -15,14 +15,16 @@ const HomePage = () => {
           <a className="p-2" href="#/">Link</a>
         </nav>
       </header>
-      <main className="bg-white flex justify-between align-center rounded-lg shadow w-100">
-        <div className="w-1/2 border-white border-2 rounded-lg">
-          <img src={weedJarPhoto} alt="Cannabis jar on kitchen counter" className="rounded-tl-lg rounded-bl-lg" />
-        </div>
-        <div className="py-4 px-6 w-1/2">
-        <h1>Affordable cannabis for all</h1>
-          <p>The only place that is actually here to help you get all your favorite cannabis products and prescriptions at prices you can afford and sustain.</p>
-        </div>
+      <main className="bg-white rounded-lg shadow">
+        <section className="flex justify-between align-center">
+          <div className="w-1/2 border-white border-2 rounded-lg">
+            <img src={weedJarPhoto} alt="Cannabis jar on kitchen counter" className="rounded-tl-lg rounded-bl-lg flex-grow object-cover h-full" />
+          </div>
+          <div className="p-10 w-1/2 flex flex-col gap-4 justify-center">
+            <h1 className="font-bold text-green-600 text-4xl">Affordable cannabis for all</h1>
+            <p className="font-medium text-lg">The only place that is actually here to help you get all your favorite cannabis products and prescriptions at prices you can afford and sustain.</p>
+          </div>
+        </section>
       </main>
       <footer>
         <p className="text-white">&copy; Copyright 2024 AV Web Design. All rights reserved.</p>
