d3fb3f9cb546f82239db706e142d6ca44b3d75f0
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-24 11:31:28 -0400

    css/js updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,19 +5,21 @@ const HomePage = () => {
   return (
     <div className="container mx-auto py-8 flex flex-col gap-8 my-2">
       <header className="bg-white flex justify-between items-center py-4 px-6 rounded-lg shadow">
-        <a href="#" className="flex items-center justify-center p-2"><img src={logo} alt="Logo Icon" className="w-8 mr-2" /><span className="font-bold">Good</span>Mj</a>
-		<nav className="flex gap-6">
-			<a className="p-2" href="#">Link</a>
-			<a className="p-2" href="#">Link</a>
-			<a className="p-2" href="#">Link</a>
-		</nav>
+        <a href="#/" className="flex items-center justify-center p-2 text-xl">
+          <img src={logo} alt="Logo Icon" className="w-8 mr-2" /><span className="font-bold">Good</span>Mj
+        </a>
+        <nav className="flex gap-6 text-lg font-medium">
+          <a className="p-2" href="#/">Link</a>
+          <a className="p-2" href="#/">Link</a>
+          <a className="p-2" href="#/">Link</a>
+        </nav>
       </header>
       <main className="bg-white flex justify-between align-center py-4 px-6 rounded-lg shadow">
         <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
       </main>
-	  <footer>
-		<p className="text-white">&copy; Copyright 2024 AV Web Design. All rights reserved.</p>
-	  </footer>
+      <footer>
+        <p className="text-white">&copy; Copyright 2024 AV Web Design. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
