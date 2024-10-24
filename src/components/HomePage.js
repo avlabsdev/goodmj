a77fd443b2778aa45ef35ffeac90f81c745e3fe2
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-24 13:36:09 -0400

    image added, js updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../logo.svg';
+import weedJarPhoto from '../images/weed-jar.jpg';
 
 const HomePage = () => {
   return (
@@ -14,8 +15,14 @@ const HomePage = () => {
           <a className="p-2" href="#/">Link</a>
         </nav>
       </header>
-      <main className="bg-white flex justify-between align-center py-4 px-6 rounded-lg shadow">
-        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
+      <main className="bg-white flex justify-between align-center rounded-lg shadow w-100">
+        <div className="w-1/2 border-white border-2 rounded-lg">
+          <img src={weedJarPhoto} alt="Cannabis jar on kitchen counter" className="rounded-tl-lg rounded-bl-lg" />
+        </div>
+        <div className="py-4 px-6 w-1/2">
+        <h1>Affordable cannabis for all</h1>
+          <p>The only place that is actually here to help you get all your favorite cannabis products and prescriptions at prices you can afford and sustain.</p>
+        </div>
       </main>
       <footer>
         <p className="text-white">&copy; Copyright 2024 AV Web Design. All rights reserved.</p>
