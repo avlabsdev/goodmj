681aa5cdd132a67ad3d28f6e4154511a76a6a875
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-24 15:52:54 -0400

    responsive updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -31,9 +31,9 @@ const HomePage = () => {
             <img src={weedJarPhoto} alt="Cannabis jar on kitchen counter" className="rounded-tl-lg rounded-bl-lg flex-grow object-cover h-full" />
           </div>
           <div className="p-20 w-1/2 flex flex-col gap-4 justify-center items-center relative">
-            <img src={givingHandPhoto} alt="Giving hands with cannabis leaf in palm" className="absolute w-1/5 -top-2 right-24" />
-            <h1 className="custom-text-green text-7xl font-bold">Affordable cannabis for all</h1>
-            <p className="font-semibold text-lg">The only place here to help you get all your cannabis products and prescriptions at prices you can afford and sustain.</p>
+            <img src={givingHandPhoto} alt="Giving hands with cannabis leaf in palm" className="absolute w-1/5 -top-2 right-24 xl:w-1/6 xl:top-4" />
+            <h1 className="custom-text-green 2xl:text-7xl font-bold xl:text-5xl">Affordable cannabis for all</h1>
+            <p className="font-semibold 2xl:text-lg xl:text-base">The only place here to help you get all your cannabis products and prescriptions at prices you can afford and sustain.</p>
             <div className="flex gap-2 mt-4 w-full">
               <a href="#/" className="py-4 bg-blue-300 text-center rounded-md custom-bg-green text-white font-medium w-full">How It Works</a>
               <a href="#/" className="py-4 bg-blue-300 text-center rounded-md custom-bg-darkblue text-white font-medium w-full">View Services</a>
