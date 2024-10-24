32521796c05e5b214322d73359c02145255d3081
Aaron Varga <hello@avui.design>
2024-10-24 00:45:21 -0400

    js updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import logo from '../logo.svg';
 
 const HomePage = () => {
   return (
-    <div class="container mx-auto py-8 flex flex-col gap-8">
-      <header class="bg-white flex justify-between align-center py-4 px-6 rounded-lg">
-        <h1><span class="font-bold">Good</span>Mj</h1>
-		<nav class="flex gap-6">
-			<a href="#">Link</a>
-			<a href="#">Link</a>
-			<a href="#">Link</a>
+    <div className="container mx-auto py-8 flex flex-col gap-8 my-2">
+      <header className="bg-white flex justify-between items-center py-4 px-6 rounded-lg shadow">
+        <a href="#" className="flex items-center justify-center p-2"><img src={logo} alt="Logo Icon" className="w-8 mr-2" /><span className="font-bold">Good</span>Mj</a>
+		<nav className="flex gap-6">
+			<a className="p-2" href="#">Link</a>
+			<a className="p-2" href="#">Link</a>
+			<a className="p-2" href="#">Link</a>
 		</nav>
       </header>
-      <main class="bg-white flex justify-between align-center py-4 px-6 rounded-lg">
+      <main className="bg-white flex justify-between align-center py-4 px-6 rounded-lg shadow">
         <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
       </main>
 	  <footer>
-		<p class="text-white">&copy; Copyright 2024 AV Web Design. All rights reserved.</p>
+		<p className="text-white">&copy; Copyright 2024 AV Web Design. All rights reserved.</p>
 	  </footer>
     </div>
   );
