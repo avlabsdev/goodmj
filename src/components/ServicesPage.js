8ffd2abbd781e7cf1abf20ddf6c78a665d48e4a9
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-25 11:52:45 +0000

    added photo, page updates

--- src/components/ServicesPage.js
+++ src/components/ServicesPage.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import certIcon from '../images/certificate.svg';
+import weedLabPhoto from '../images/weed-lab.jpg';
 
 const ServicesPage = () => {
   return (
     <div className="container mx-auto py-4 flex flex-col gap-8">
       <main>
         <section className="flex justify-between gap-8">
-          <div className="p-20 w-3/5 flex flex-col gap-4 justify-center relative bg-white rounded-lg shadow">
+          <div className="p-20 w-3/5 flex flex-col gap-4 relative bg-white rounded-lg shadow">
             <div className="flex items-center gap-4">
               <img src={certIcon} alt="Certificate icon" className="w-14" />
               <h1 className="custom-text-green 2xl:text-5xl font-bold xl:text-3xl">Services & Features</h1>
             </div>
-            <p className="custom-text-darkblue font-semibold 2xl:text-xl xl:text-lg">We have an ever-growing list of services and features to offer
-            you with affordable pricing options that work for you.</p>
+            <h2 className="custom-text-darkblue font-semibold 2xl:text-xl xl:text-lg">We have an ever-growing list of services and features to offer
+              you with affordable pricing options that work for you.</h2>
             <p>*This is more text to explain the below content.</p>
           </div>
           <div className="w-2/5">
-            <p>Hello!</p>
+          <img src={weedLabPhoto} alt="Cannabis lab" className="rounded-lg shadow border-2 border-white" />
+            <h2>We only work with companies that
+              pride themselves on proper lab
+              testing and quality assurance.</h2>
+            <p>Our promise is to provide you with the highest
+              quality cannabis for the lowest costs available.</p>
+            <p>We guarantee high-quality products that
+              go through rigorous lab testing and quality
+              assurance.</p>
             <div className="flex gap-2 mt-4 w-full">
-              <a href="#/" className="py-4 text-center rounded-md custom-bg-green custom-text-darkblue font-medium w-full">How It Works</a>
-              <a href="#/" className="py-4 text-center rounded-md custom-text-darkblue bg-white font-medium w-full">View Services</a>
+              <a href="/plans" className="py-4 text-center rounded-md custom-bg-green custom-text-darkblue font-medium w-full shadow">View Plans</a>
+              <a href="/menu" className="py-4 text-center rounded-md custom-text-darkblue bg-white font-medium w-full shadow">View Menu</a>
             </div>
           </div>
         </section>
