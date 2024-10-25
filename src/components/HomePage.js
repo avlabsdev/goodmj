dde1a924ab0b5fe60167dbd879c358536f058e2a
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-25 15:19:39 +0000

    responsive updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,14 +6,14 @@ const HomePage = () => {
   return (
     <div className="container mx-auto py-4 flex flex-col gap-8">
       <main className="bg-white rounded-lg shadow">
-        <section className="flex justify-between items-center">
-          <div className="w-1/2 border-white border-2 rounded-lg">
+        <section className="flex lg:flex-row justify-between md:flex-col">
+          <div className="2xl:w-1/2 border-white border-2 rounded-lg md:w-full">
             <img src={weedJarPhoto} alt="Cannabis jar on kitchen counter" className="rounded-tl-lg rounded-bl-lg flex-grow object-cover h-full" />
           </div>
-          <div className="p-20 w-1/2 flex flex-col gap-4 justify-center items-center relative">
-            <img src={givingHandPhoto} alt="Giving hands with cannabis leaf in palm" className="absolute w-1/5 -top-2 right-24 xl:w-1/6 xl:top-4" />
-            <h1 className="custom-text-green 2xl:text-7xl font-bold xl:text-5xl">Affordable cannabis for all</h1>
-            <h2 className="font-semibold 2xl:text-2xl xl:text-xl custom-text-darkblue">The only place here to help you get all your cannabis products and prescriptions at prices you can afford and sustain.</h2>
+          <div className="p-20 2xl:w-1/2 md:w-full flex flex-col gap-4 justify-center relative">
+            <img src={givingHandPhoto} alt="Giving hands with cannabis leaf in palm" className="absolute 2xl:w-1/4 2xl:top-6 2xl:right-20 xl:w-1/5 xl:top-4 xl:right-24 lg:top-10 lg:w-20 md:w-24 md:top-6" />
+            <h1 className="custom-text-green 2xl:text-7xl font-bold xl:text-6xl lg:text-4xl md:text-3xl">Affordable cannabis for all</h1>
+            <h2 className="font-semibold 2xl:text-2xl xl:text-xl custom-text-darkblue lg:text-lg">The only place here to help you get all your cannabis products and prescriptions at prices you can afford and sustain.</h2>
             <div className="flex gap-2 mt-4 w-full">
               <a href="#/" className="py-4 text-center rounded-md custom-bg-green text-white font-medium w-full shadow">How It Works</a>
               <a href="/services" className="py-4 text-center rounded-md custom-bg-darkblue text-white font-medium w-full shadow">View Services</a>
