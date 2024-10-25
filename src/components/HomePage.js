ee740706e475f71f4e017470c70bf73de47da8ad
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-25 11:20:08 +0000

    page updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -13,10 +13,10 @@ const HomePage = () => {
           <div className="p-20 w-1/2 flex flex-col gap-4 justify-center items-center relative">
             <img src={givingHandPhoto} alt="Giving hands with cannabis leaf in palm" className="absolute w-1/5 -top-2 right-24 xl:w-1/6 xl:top-4" />
             <h1 className="custom-text-green 2xl:text-7xl font-bold xl:text-5xl">Affordable cannabis for all</h1>
-            <p className="font-semibold 2xl:text-lg xl:text-base">The only place here to help you get all your cannabis products and prescriptions at prices you can afford and sustain.</p>
+            <p className="font-semibold 2xl:text-2xl xl:text-xl">The only place here to help you get all your cannabis products and prescriptions at prices you can afford and sustain.</p>
             <div className="flex gap-2 mt-4 w-full">
               <a href="#/" className="py-4 text-center rounded-md custom-bg-green text-white font-medium w-full">How It Works</a>
-              <a href="#/" className="py-4 text-center rounded-md custom-bg-darkblue text-white font-medium w-full">View Services</a>
+              <a href="/services" className="py-4 text-center rounded-md custom-bg-darkblue text-white font-medium w-full">View Services</a>
             </div>
           </div>
         </section>
--- src/components/ServicesPage.js
+++ src/components/ServicesPage.js
@@ -5,17 +5,21 @@ const ServicesPage = () => {
   return (
     <div className="container mx-auto py-4 flex flex-col gap-8">
       <main>
-        <section className="flex justify-between items-center">
+        <section className="flex justify-between gap-8">
           <div className="p-20 w-3/5 flex flex-col gap-4 justify-center relative bg-white rounded-lg shadow">
             <div className="flex items-center gap-4">
               <img src={certIcon} alt="Certificate icon" className="w-14" />
               <h1 className="custom-text-green 2xl:text-5xl font-bold xl:text-3xl">Services & Features</h1>
             </div>
-            <p className="custom-text-darkblue font-semibold 2xl:text-lg xl:text-base">The only place here to help you get all your cannabis products and prescriptions at prices you can afford and sustain.</p>
+            <p className="custom-text-darkblue font-semibold 2xl:text-xl xl:text-lg">We have an ever-growing list of services and features to offer
+            you with affordable pricing options that work for you.</p>
             <p>*This is more text to explain the below content.</p>
+          </div>
+          <div className="w-2/5">
+            <p>Hello!</p>
             <div className="flex gap-2 mt-4 w-full">
-              <a href="#/" className="py-4 text-center rounded-md custom-bg-green text-white font-medium w-full">How It Works</a>
-              <a href="#/" className="py-4 text-center rounded-md custom-bg-darkblue text-white font-medium w-full">View Services</a>
+              <a href="#/" className="py-4 text-center rounded-md custom-bg-green custom-text-darkblue font-medium w-full">How It Works</a>
+              <a href="#/" className="py-4 text-center rounded-md custom-text-darkblue bg-white font-medium w-full">View Services</a>
             </div>
           </div>
         </section>
