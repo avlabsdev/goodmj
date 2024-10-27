67675d04026d583fdab6125d92c4effaed91fd5f
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-27 03:33:56 +0000

    page updates

--- src/components/PlansPage.js
+++ src/components/PlansPage.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPeopleLine, faAward, faCreditCardFront } from '@awesome.me/kit-bc1b6271da/icons/classic/solid'
 
 import plansHeroGfx from '../images/plans-hero-gfx.png';
 import checkIcon from '../images/check-icon.svg';
-import groupIcon from '../images/people-line-solid.svg';
-import ribbonIcon from '../images/award-sharp-solid.svg';
-import creditCardIcon from '../images/credit-card.svg';
 
 const PlansPage = () => {
   return (
@@ -19,7 +18,7 @@ const PlansPage = () => {
         </section>
         <section className="flex justify-between items-start gap-8">
           <div className="w-1/3">
-            <h2 className="w-full custom-bg-darkblue text-white p-6 text-center rounded-tl-lg rounded-tr-lg border-2 border-white font-medium flex items-center justify-center gap-3 border-b-0 text-lg"><img src={groupIcon} alt="Community plan icon" className="w-8" />Community Plan</h2>
+            <h2 className="w-full custom-bg-darkblue text-white p-6 text-center rounded-tl-lg rounded-tr-lg border-2 border-white font-medium flex items-center justify-center gap-3 border-b-0 text-lg"><FontAwesomeIcon icon={faPeopleLine} className="custom-text-green text-2xl" />Community Plan</h2>
             <div className="p-2 custom-bg-yellow text-center font-medium uppercase text-sm border-l-2 border-r-2 border-white">
               $100 Per Month
             </div>
@@ -36,7 +35,7 @@ const PlansPage = () => {
             </div>
           </div>
           <div className="w-1/3">
-            <h2 className="w-full custom-bg-darkblue text-white p-6 text-center rounded-tl-lg rounded-tr-lg border-2 border-white font-medium flex items-center justify-center gap-3 border-b-0 text-lg"><img src={ribbonIcon} alt="Community plan icon" className="w-5" />Top Tier Plan</h2>
+            <h2 className="w-full custom-bg-darkblue text-white p-6 text-center rounded-tl-lg rounded-tr-lg border-2 border-white font-medium flex items-center justify-center gap-3 border-b-0 text-lg"><FontAwesomeIcon icon={faAward} className="custom-text-green text-2xl" />Top Tier Plan</h2>
             <div className="p-2 custom-bg-yellow text-center font-medium uppercase text-sm border-l-2 border-r-2 border-white">
               $200 Per Month
             </div>
@@ -53,7 +52,7 @@ const PlansPage = () => {
             </div>
           </div>
           <div className="w-1/3">
-            <h2 className="w-full custom-bg-darkblue text-white p-6 text-center rounded-tl-lg rounded-tr-lg border-2 border-white font-medium flex items-center justify-center gap-3 border-b-0 text-lg"><img src={creditCardIcon} alt="Community plan icon" className="w-6" />Pay-As-You-Go Perk Plan</h2>
+            <h2 className="w-full custom-bg-darkblue text-white p-6 text-center rounded-tl-lg rounded-tr-lg border-2 border-white font-medium flex items-center justify-center gap-3 border-b-0 text-lg"><FontAwesomeIcon icon={faCreditCardFront} className="custom-text-green text-2xl" />Pay-As-You-Go Perk Plan</h2>
             <div className="p-2 custom-bg-yellow text-center font-medium uppercase text-sm border-l-2 border-r-2 border-white">
               $20 Per Month
             </div>
