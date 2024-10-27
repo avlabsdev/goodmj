ad1461c8ccd2f67f8b48553477de3f7e4388ed36
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-27 04:12:00 +0000

    page updates, added assets

--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import healthIcon from '../images/health.svg';
+
 const HelpPage = () => {
   return (
     <div className="container mx-auto py-4 flex flex-col gap-8 px-4">
       <main className="bg-white rounded-lg shadow">
         <section className="flex justify-between items-center">
           <div className="p-20 w-full flex flex-col gap-4 justify-center relative">
-            <h1 className="custom-text-green 2xl:text-5xl font-bold xl:text-5xl">Help & Support</h1>
+            <h1 className="custom-text-green 2xl:text-5xl font-bold xl:text-5xl flex items-center gap-4"><img src={healthIcon} alt="Heart icon" className="w-14" />Help & Support</h1>
             <p className="font-semibold 2xl:text-lg xl:text-base custom-text-darkblue">Need help? We have you covered with our 24/7 support team.
             </p>
           </div>
--- src/components/StrainfinderPage.js
+++ src/components/StrainfinderPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import microscopeIcon from '../images/microscope.svg';
+
 const StrainfinderPage = () => {
   return (
     <div className="container mx-auto py-4 flex flex-col gap-8 px-4">
       <main className="bg-white rounded-lg shadow">
         <section className="flex justify-between items-center">
           <div className="p-20 w-full flex flex-col gap-4 justify-center relative">
-            <h1 className="custom-text-green 2xl:text-5xl font-bold xl:text-5xl">Strainfinder&trade;</h1>
+            <h1 className="custom-text-green 2xl:text-5xl font-bold xl:text-5xl flex items-center gap-4"><img src={microscopeIcon} alt="Microscope icon" className="w-14" />Strainfinder&trade;</h1>
             <p className="font-semibold 2xl:text-lg xl:text-base custom-text-darkblue">Search any cannabis strain or product to learn more..</p>
           </div>
         </section>
