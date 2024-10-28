9f6c3b143d58963aa01c2ef2bacb0a66f1d45e3d
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-28 16:41:44 +0000

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="container mx-auto py-4 flex flex-col gap-8 mb-6 px-4">
       <footer className="flex justify-between items-center px-4">
         <p className="text-white">&copy; Copyright 2024 <a href="https://avweb.io" target="_blank" rel="noreferrer" className="underline hover:no-underline">AV Web Design</a>. All rights reserved.</p>
-        <a href="https://github.com/avwebio" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} className="text-xl text-white" /></a>
+        <a href="https://github.com/avwebio" target="_blank" rel="noreferrer" aria-label="GitHub"><FontAwesomeIcon icon={faGithub} className="text-xl text-white" /></a>
       </footer>
     </div>
   );
