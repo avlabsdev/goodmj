0557fad69b446b172da3e78e5a8253a8763deaa6
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-29 23:32:19 +0000

    help page updates

--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -4,16 +4,20 @@ import healthIcon from '../images/health.svg';
 
 const HelpPage = () => {
   return (
-    <div className="container mx-auto py-4 flex flex-col gap-8 px-4">
-      <main className="bg-white rounded-lg shadow">
-        <section className="flex justify-between items-center">
+    <div className="container mx-auto py-4 flex gap-8 px-4">
+        <section className="flex justify-between items-center bg-white rounded-lg shadow w-1/2">
           <div className="p-20 w-full flex flex-col gap-4 justify-center relative">
             <h1 className="custom-text-green 2xl:text-5xl font-bold xl:text-5xl flex items-center gap-4"><img src={healthIcon} alt="Heart icon" className="w-14" />Help & Support</h1>
-            <p className="custom-text-darkblue font-semibold 2xl:text-xl xl:text-lg">Need help? We have you covered with our 24/7 support team.
+            <p className="custom-text-darkblue font-semibold 2xl:text-xl xl:text-lg">We have you covered with our 24/7 support team.
             </p>
+            <p className="my-4 text-sm text-gray-600">*Submit a trouble ticket below, give us a call, or start a chat.</p>
           </div>
         </section>
-      </main>
+        <section className="flex justify-between items-center bg-white rounded-lg shadow w-1/2">
+          <div className="p-20 w-full flex flex-col gap-4 justify-center relative">
+            Image w/ buttons
+          </div>
+        </section>
     </div>
   );
 }
