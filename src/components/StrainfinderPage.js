7c299835333e9e5aeaa71c3a5b69332c4ed6dff5
Aaron Varga <aaronvarga@myyahoo.com>
2024-10-29 11:34:02 +0000

    page update

--- src/components/StrainfinderPage.js
+++ src/components/StrainfinderPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@awesome.me/kit-bc1b6271da/icons/classic/solid'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faMagnifyingGlass } from '@awesome.me/kit-bc1b6271da/icons/classic/solid'
 
 import microscopeIcon from '../images/microscope.svg';
 import strainfinderHeroImage from '../images/strainfinder-hero-comp.png';
