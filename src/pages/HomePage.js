2dbffae4ff6a72093823ce0e2c1e0b177db1ddbf
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-08 20:52:57 -0500

    homepage updates

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -14,11 +14,11 @@ const HomePage = () => {
               className="lg:rounded-tl-lg lg:rounded-bl-lg flex-grow object-cover h-full lg:rounded-tr-none rounded-tr-lg rounded-bl-none rounded-tl-lg object-fit"
             />
           </div>
-          <div className="2xl:text-left xl:text-left lg:text-left md:p-20 2xl:w-1/2 md:w-full flex flex-col gap-4 justify-center relative sm:text-center p-5 lg:py-32">
+          <div className="2xl:text-left xl:text-left lg:text-left md:p-20 2xl:w-1/2 md:w-full flex flex-col gap-4 justify-center relative sm:text-center p-5 2xl:py-48 xl:py-36 lg:py-32 md:py-8">
             <img
               src={givingHandPhoto}
               alt="Giving hands with cannabis leaf in palm"
-              className="2xl:absolute xl:absolute lg:absolute 2xl:w-1/4 2xl:top-8 2xl:right-20 xl:w-1/5 xl:top-12 xl:right-20 lg:top-16 lg:w-1/5 lg:right-20 md:w-1/3 md:relative md:top-0 mx-auto sm:w-1/3 xs:w-1/5 w-1/3 relative sm:relative sm:top-0 top-0 right-0"
+              className="2xl:absolute xl:absolute lg:absolute 2xl:w-1/4 2xl:top-16 2xl:right-20 xl:w-1/5 xl:top-16 xl:right-20 lg:top-16 lg:w-1/5 lg:right-20 md:w-1/3 md:relative md:top-0 mx-auto sm:w-1/3 xs:w-1/5 w-1/3 relative sm:relative sm:top-0 top-0 right-0"
             />
             <h1 className="custom-text-green 2xl:text-7xl lg:text-left font-bold xl:text-6xl lg:text-5xl md:text-6xl sm:text-6xl text-4xl sm:text-center text-center md:text-center">
               Affordable cannabis for all
