a06a20e36debe346a16c2eb5ccee4cc91d7ff8b8
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-10 18:23:30 -0500

    coded simple animations for call/chat buttons. Need to restructure and use a different method - currently working on it. Will add end call/chat button as well.

--- src/pages/HelpPage.js
+++ src/pages/HelpPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -10,7 +10,56 @@ import healthIcon from "../images/health.svg";
 import operatorsImage from "../images/operators.jpg";
 import HelpForm from "../components/HelpForm";
 
+import ring from "../audio/ring.wav";
+import voiceHelp from "../audio/voice-help.wav";
+
 const HelpPage = () => {
+  function handleCallClick() {
+    const phoneBtn = document.querySelector("#phone-call-btn");
+    const chatBtn = document.querySelector("#chat-start-btn");
+    phoneBtn.textContent = "Calling...";
+    const audio = new Audio(ring);
+    audio.play();
+    setTimeout(() => {
+      phoneBtn.textContent = "Connected!";
+    }, 3000);
+
+    setTimeout(() => {
+      const audio = new Audio(voiceHelp);
+      audio.play();
+      phoneBtn.textContent = `Hello, how can I help?`;
+      phoneBtn.classList.add("w-full");
+      // phoneBtn.style.backgroundColor = "#ef4444";
+      phoneBtn.classList.add("scale-up-hor-left");
+      chatBtn.classList.add("hidden");
+      // go from 2xl width down to sm and mobile ensuring border-radius changes
+    }, 4000);
+
+    // setTimeout(() => {
+    //   phoneBtn.textContent = `End Call`;
+    // }, 4000);
+  }
+  function handleChatClick() {
+    const phoneBtn = document.querySelector("#phone-call-btn");
+    const chatBtn = document.querySelector("#chat-start-btn");
+    chatBtn.textContent = "Connecting...";
+    setTimeout(() => {
+      chatBtn.textContent = "Connected!";
+    }, 3000);
+
+    setTimeout(() => {
+      chatBtn.textContent = `Hello, how can I help?`;
+      chatBtn.classList.add("w-full");
+      // phoneBtn.style.backgroundColor = "#ef4444";
+      chatBtn.classList.add("scale-up-hor-right");
+      phoneBtn.classList.add("hidden");
+      // go from 2xl width down to sm and mobile ensuring border-radius changes
+    }, 4000);
+
+    // setTimeout(() => {
+    //   phoneBtn.textContent = `End Call`;
+    // }, 4000);
+  }
   return (
     <div className="container mx-auto flex gap-2 flex-col lg:flex-row">
       <section className="flex justify-between items-center bg-white rounded-lg shadow lg:w-1/2">
@@ -33,12 +82,15 @@ const HelpPage = () => {
           <img
             src={operatorsImage}
             alt="Tech support operators"
-            className="object-cover h-96 w-full lg:h-full rounded-lg"
+            className="object-cover h-96 w-full lg:h-full rounded-lg border-r-2 border-l-2 border-t-2"
           />
-          <div className="bg-white rounded-lg flex w-full sm:flex-row flex-col absolute right-0 left-0 -top-0.5 lg:top-auto lg:bottom-0">
+          <div className="bg-white rounded-lg rounded-tl-none rounded-tr-none flex w-full sm:flex-row flex-col absolute right-0 left-0 -top-0.5 lg:top-auto lg:bottom-0 border-2 border-white items-center">
             <a
               href="#/"
-              className="py-4 text-center lg:rounded-bl-lg rounded-tr-lg lg:rounded-tr-none rounded-tl-lg lg:rounded-tl-none sm:rounded-tr-none md:rounded-tr-none custom-bg-darkblue text-white font-medium w-full shadow border-2 border-white flex items-center justify-center gap-2"
+              className="py-4 text-center lg:rounded-bl-lg rounded-tr-lg lg:rounded-tr-none rounded-tl-lg lg:rounded-tl-none sm:rounded-tr-none md:rounded-tr-none custom-bg-darkblue text-white font-medium w-full shadow flex items-center justify-center gap-2 border-0"
+              onClick={handleCallClick}
+              id="phone-call-btn"
+              // use first button for inital events, then unhide the red button behind the front button for the ability to end call and return to the two button options again for call or chat
             >
               <FontAwesomeIcon
                 icon={faPhoneOffice}
@@ -46,16 +98,26 @@ const HelpPage = () => {
               />
               Start a Call
             </a>
-            <a
+            {/* <a
+              href="#/"
+              className="hidden py-4 text-center lg:rounded-bl-lg rounded-tr-lg lg:rounded-tr-none rounded-tl-lg lg:rounded-tl-none sm:rounded-tr-none md:rounded-tr-none bg-red-500 text-white font-medium w-full shadow items-center justify-center gap-2 border-0"
+              // onClick={endCallClick}
+              id="end-call-btn"
+            >
+              End Call
+            </a> */}
+            <button
               href="/services"
-              className="py-4 text-center bg-white custom-text-darkblue font-medium w-full shadow lg:rounded-br-lg rounded-tr-lg lg:rounded-tr-none border-t-2 flex items-center justify-center gap-2"
+              onClick={handleChatClick}
+              className="py-4 text-center bg-white custom-text-darkblue font-medium w-full shadow lg:rounded-br-lg rounded-tr-lg lg:rounded-tr-none flex items-center justify-center gap-2"
+              id="chat-start-btn"
             >
               <FontAwesomeIcon
                 icon={faHeadset}
                 className="custom-text-green text-xl"
               />
               Start a Chat
-            </a>
+            </button>
           </div>
         </div>
       </section>
