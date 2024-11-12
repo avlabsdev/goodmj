ebadb828b2dc089219dfb2ba0594c7460ca18a72
Aaron Varga <aaronvarga@myyahoo.com>
2024-11-12 18:22:40 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,14 +7,14 @@ const Footer = () => {
     <div className="container mx-auto py-4 flex flex-col gap-8 m-8 px-4">
       <footer className="flex flex-col sm:flex-row px-0 justify-between items-center sm:px-4 gap-8 sm:gap-0">
         <p className="text-white text-sm sm:text-base text-center sm:text-left">
-          &copy; Copyright 2024{" "}
+          &copy;{" "}
           <a
             href="https://github.com/avlabsdev"
             target="_blank"
             rel="noreferrer"
             className="underline hover:no-underline"
           >
-            AV Labs
+            AV Labs 2024
           </a>
           . All rights reserved.
         </p>
